b33dab848120dca100950f036b02d67ff00d9cf4
sdey3 <sdey@edms.co.in>
2014-08-18 10:10:33 +0530

    a commit from TOMODO - JS File

---
+++ JS.js
@@ -0,0 +1,29 @@
+/**
+ * TOMODO
+ * 
+ * This file holds the user script written by YOU.
+ * It only appears in Preview mode and its purpose is to allow you to debug your script.
+ * Put your breakpoints below.
+ * 
+ * Thanks,
+ * the TOMODO Team.  
+ */
+
+function TOMDO_runUserScript(){
+	//----------------------------------- Your Code Here ------------------------------------	
+	/**********************************************
+Add your Javascript code below.
+
+To debug your code use Preview mode and
+debug tomodouserscript.js using
+Chrome Developer Tools, Firebug or another Javascript debugger.
+**********************************************/
+	
+	//---------------------------------------------------------------------------------------	
+}
+
+function TOMODO_runWYSIWYGScript(){
+	//----------------------------------- WYSIWYG Auto generated code -----------------------
+	
+	//---------------------------------------------------------------------------------------
+}
